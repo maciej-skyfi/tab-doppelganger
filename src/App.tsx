af761b29954593254473f8b6d2e9c43fd57ecb74
Maciej Kwas <admin@prowseed.com>
2023-10-01 22:49:47 +0200

    build 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,78 @@
+import { useAsyncRetry } from "react-use";
+import { pickBy, groupBy, values, flatten } from "ramda";
+import { MdOutlineCleanHands } from "react-icons/md";
+
 import "./assets/css/App.css";
 
 function App() {
-  return <div className="container">Hello World!</div>;
+  const state = useAsyncRetry(async () => {
+    return await chrome.tabs.query({ currentWindow: true });
+  });
+
+  const closeTab = (tabId: number) => {
+    chrome.tabs.remove(tabId);
+    state.retry();
+  };
+
+  const goToTab = (tabId: number) => {
+    chrome.tabs.update(tabId, { active: true });
+  };
+
+  if (state.loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (state.error) {
+    return <div>Error: {state.error.message}</div>;
+  }
+
+  const tabs = state.value ?? [];
+
+  const duplicatedOriginsGrouped = pickBy(
+    (x) => x.length > 1,
+    groupBy(
+      (tab: chrome.tabs.Tab) => (tab.url ? new URL(tab.url).origin : "x"),
+      tabs
+    )
+  );
+
+  const duplicatedOriginsGroupedFlattened = flatten(
+    values(duplicatedOriginsGrouped as any)
+  );
+
+  if (duplicatedOriginsGroupedFlattened.length === 0) {
+    return (
+      <div className="container container-empty">
+        <p>Seems like you keep'em clean those tabs!</p>
+        <div>
+          <MdOutlineCleanHands />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Tabs with same URL origin ({duplicatedOriginsGroupedFlattened.length})
+      </h1>
+      <div className="list">
+        {duplicatedOriginsGroupedFlattened.map((tab) => (
+          <div key={tab.id} className="listitem">
+            <div className="listitem-icon">
+              <img alt="" src={tab.favIconUrl} />
+            </div>
+            <div onClick={() => goToTab(tab.id)} className="listitem-name">
+              {tab.title?.substring(0, 80)}
+            </div>
+            <button onClick={() => closeTab(tab.id)} className="listitem-close">
+              &times;
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,63 @@
-console.log("background");
+function compareUrls(left: string, right: string) {
+  const urlA = new URL(left);
+  const urlB = new URL(right);
+  return urlA.origin === urlB.origin;
+}
+
+async function onCompleted(details: any) {
+  if (
+    details.frameId == 0 &&
+    details.url &&
+    details.url != "" &&
+    !details.url.match(/^chrome:\/\//)
+  ) {
+    chrome.tabs.get(details.tabId, async function (tab) {
+      if (typeof tab == "undefined") return null;
+      if (tab.url?.match(/^view-source:/)) return null;
+
+      const tabs = await chrome.tabs.query({ currentWindow: true });
+
+      const duplicates = tabs.filter(function (t) {
+        return (
+          compareUrls(t.url ?? "", details.url) && t.id != tab.id && !t.pinned
+        );
+      });
+      if (duplicates.length) {
+        removeDuplicates(tab, duplicates);
+      }
+    });
+  }
+
+  function removeDuplicates(
+    tab: chrome.tabs.Tab,
+    duplicates: chrome.tabs.Tab[]
+  ) {
+    const title = `Found ${duplicates.length} tab(s) with same URL`;
+    const message = new URL(tab.url ?? "").hostname;
+    const options = {
+      type: "basic" as chrome.notifications.TemplateType,
+      iconUrl: chrome.runtime.getURL("assets/icons/icon48.png"),
+      title: title,
+      message: message,
+      contextMessage: "Click here to clean them up",
+      isClickable: true,
+    };
+
+    let notificationId: string | null = null;
+
+    chrome.notifications.create("", options, function (nId) {
+      notificationId = nId;
+    });
+
+    chrome.notifications.onClicked.addListener(function (nId) {
+      if (nId !== notificationId) return;
+      duplicates.forEach(function (duplicate) {
+        duplicate.id && chrome.tabs.remove(duplicate.id);
+      });
+      chrome.notifications.clear(notificationId, function () {});
+      return false;
+    });
+  }
+}
+
+chrome.webNavigation.onCompleted.addListener(onCompleted);
